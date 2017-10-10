590afab48c91cf1d8115ef9090cd6b221fb9fcca
Guja Babunashvili <Guja1501@gmail.com>
2017-10-10 19:36:49 +0400

    Add methods ON/OFF

--- src/SensorAFK.js
+++ src/SensorAFK.js
@@ -1,26 +1,32 @@
 module.exports = class SensorAFK {
 	constructor(options = {}) {
 		SensorAFK.initStatic();
-		let { time, sensors, node } = SensorAFK.filterOptions(options);
+		let {time, sensors, node, callback} = SensorAFK.filterOptions(options);
 
 		this.node = node;
 		this.time = time;
+		this.callback = callback;
 		this.sensors = Array.isArray(sensors) ? sensors : sensors.split(' ');
 		this.timeout = null;
+
+		this.statusOn = true;
 
 		this.flush();
 		this.eventRegistration();
 	}
 
-	static initStatic(){
-		if(this.initStaticDone === true)
+	static initStatic() {
+		if (this.initStaticDone === true)
 			return true;
 
 		this.initStaticDone = true;
 		this.defaultOptions = {
 			time: 30,
 			sensors: 'click wheel mousemove keydown keyup keypress',
-			node: document
+			node: document,
+			callback: () => {
+				// default callback does nothing
+			}
 		};
 
 		this.events = {
@@ -31,28 +37,35 @@ module.exports = class SensorAFK {
 		};
 	}
 
-	static filterOptions(options = {}){
+	static filterOptions(options = {}) {
 		return Object.assign({}, SensorAFK.defaultOptions, options);
 	}
 
-	trigger(event){
+	get seconds() {
+		return this.time * 1000;
+	}
+
+	trigger(event) {
 		this.node.dispatchEvent(event);
 	}
 
 	flush() {
-		this.trigger(SensorAFK.events.flush);
-		this.stop().start();
+		if (this.statusOn) {
+			this.trigger(SensorAFK.events.flush);
+			this.stop().start();
+		}
 	}
 
 	stop() {
 		this.trigger(SensorAFK.events.stop);
+
 		if (this.timeout !== null) {
 			try {
 				clearTimeout(this.timeout);
 			} catch (e) {
+			} finally {
+				this.timeout = null;
 			}
-
-			this.timeout = null;
 		}
 
 		return this;
@@ -60,22 +73,41 @@ module.exports = class SensorAFK {
 
 	start() {
 		this.trigger(SensorAFK.events.start);
+
 		this.timeout = setTimeout(() => {
 			this.afk();
-		}, this.time);
+		}, this.seconds);
 
 		return this;
 	}
 
 	afk() {
+		if (!this.statusOn) {
+			return this;
+		}
+
 		this.trigger(SensorAFK.events.afk);
+
+		this.callback();
 
 		return this;
 	}
 
-	eventRegistration(){
-		for(let sensor of this.sensors) {
+	eventRegistration() {
+		for (let sensor of this.sensors) {
 			this.node.addEventListener(sensor, this.flush.bind(this), false);
 		}
+	}
+
+	off() {
+		this.statusOn = false;
+	}
+
+	on() {
+		this.statusOn = true;
+	}
+
+	toggle() {
+		this.statusOn = !this.statusOn;
 	}
 };
